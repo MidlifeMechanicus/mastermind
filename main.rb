dd7a7fa0a0e049eb362319f6e765c3f67cd940a0
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-16 07:56:23 +0100

    Create basic board image for console

---
+++ main.rb
@@ -0,0 +1,6 @@
+# Will need board
+# Will need target array and 12 guess arrays and 12 feedback arrays with logic
+# Will have to implement colors!
+# Will need instructions!
+
+puts "======= MASTERMIND =======\n| ? || ? || ? || ? ||CLUE|\n==========================\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n|   ||   ||   ||   ||    |\n=========================="
