1d2e00aec48da337118fa56ea6f73e78c57e86ee
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-20 09:39:25 +0100

    Refactor PlayGame to loop through game rounds in preparation for handling two modes

--- lib/modes/board/play_game.rb
+++ lib/modes/board/play_game.rb
@@ -1,98 +1,108 @@
 module PlayGame
   def play_game
-    p self.code
-    get_choices
-    get_clue self.round1
-    format_data self.round1
-    show_board
-    check_win self.round1
+    game_rounds = [self.round1, self.round2, self.round3, self.round4, self.round5, self.round6, self.round7, self.round8, self.round9, self.round10, self.round11, self.round12]
+    p game_rounds
 
-    if winner == false
-      get_choices
-      get_clue self.round2
-      format_data self.round2
-      show_board
-      check_win self.round2
+    game_rounds.each do |n|
+      play_code_round n
     end
 
-    if winner == false
-      get_choices
-      get_clue self.round3
-      format_data self.round3
-      show_board
-      check_win self.round3
-    end
+    #play_code_round game_rounds[0]
 
-    if winner == false
-      get_choices
-      get_clue self.round4
-      format_data self.round4
-      show_board
-      check_win self.round4
-    end
 
-    if winner == false
-      get_choices
-      get_clue self.round5
-      format_data self.round5
-      show_board
-      check_win self.round5
-    end
+    # p self.code
+    # get_choices
+    # get_clue self.round1
+    # format_data self.round1
+    # show_board
+    # check_win self.round1
+# 
+    # if winner == false
+      # get_choices
+      # get_clue self.round2
+      # format_data self.round2
+      # show_board
+      # check_win self.round2
+    # end
+# 
+    # if winner == false
+      # get_choices
+      # get_clue self.round3
+      # format_data self.round3
+      # show_board
+      # check_win self.round3
+    # end
+# 
+    # if winner == false
+      # get_choices
+      # get_clue self.round4
+      # format_data self.round4
+      # show_board
+      # check_win self.round4
+    # end
+# 
+    # if winner == false
+      # get_choices
+      # get_clue self.round5
+      # format_data self.round5
+      # show_board
+      # check_win self.round5
+    # end
+# 
+    # if winner == false
+      # get_choices
+      # get_clue self.round6
+      # format_data self.round6
+      # show_board
+      # check_win self.round6
+    # end
+# 
+    # if winner == false
+      # get_choices
+      # get_clue self.round7
+      # format_data self.round7
+      # show_board
+      # check_win self.round7
+    # end
 
-    if winner == false
-      get_choices
-      get_clue self.round6
-      format_data self.round6
-      show_board
-      check_win self.round6
-    end
+    # if winner == false
+      # get_choices
+      # get_clue self.round8
+      # format_data self.round8
+      # show_board
+      # check_win self.round8
+    # end
+# 
+    # if winner == false
+      # get_choices
+      # get_clue self.round9
+      # format_data self.round9
+      # show_board
+      # check_win self.round9
+    # end
+# 
+    # if winner == false
+      # get_choices
+      # get_clue self.round10
+      # format_data self.round10
+      # show_board
+      # check_win self.round10
+    # end
+# 
+    # if winner == false
+      # get_choices
+      # get_clue self.round11
+      # format_data self.round11
+      # show_board
+      # check_win self.round11
+    # end
 
-    if winner == false
-      get_choices
-      get_clue self.round7
-      format_data self.round7
-      show_board
-      check_win self.round7
-    end
-
-    if winner == false
-      get_choices
-      get_clue self.round8
-      format_data self.round8
-      show_board
-      check_win self.round8
-    end
-
-    if winner == false
-      get_choices
-      get_clue self.round9
-      format_data self.round9
-      show_board
-      check_win self.round9
-    end
-
-    if winner == false
-      get_choices
-      get_clue self.round10
-      format_data self.round10
-      show_board
-      check_win self.round10
-    end
-
-    if winner == false
-      get_choices
-      get_clue self.round11
-      format_data self.round11
-      show_board
-      check_win self.round11
-    end
-
-    if winner == false
-      get_choices
-      get_clue self.round12
-      format_data self.round12
-      show_board
-      check_win self.round12
-    end  
+    # if winner == false
+      # get_choices
+      # get_clue self.round12
+      # format_data self.round12
+      # show_board
+      # check_win self.round12
+    # end  
   end
 end
--- main.rb
+++ main.rb
@@ -11,19 +11,22 @@ include GetMode
 # Will need the above after testing
 
 new_board = CodeBoard.new
+new_board.play_game
 
-new_board.play_code_round new_board.round1
-new_board.play_code_round new_board.round2
-new_board.play_code_round new_board.round3
-new_board.play_code_round new_board.round4
-new_board.play_code_round new_board.round5
-new_board.play_code_round new_board.round6
-new_board.play_code_round new_board.round7
-new_board.play_code_round new_board.round8
-new_board.play_code_round new_board.round9
-new_board.play_code_round new_board.round10
-new_board.play_code_round new_board.round11
-new_board.play_code_round new_board.round12
+# new_board.play_code_round new_board.round1
+# new_board.play_code_round new_board.round2
+# new_board.play_code_round new_board.round3
+# new_board.play_code_round new_board.round4
+# new_board.play_code_round new_board.round5
+# new_board.play_code_round new_board.round6
+# new_board.play_code_round new_board.round7
+# new_board.play_code_round new_board.round8
+# new_board.play_code_round new_board.round9
+# new_board.play_code_round new_board.round10
+# new_board.play_code_round new_board.round11
+# new_board.play_code_round new_board.round12
+# Create loop with cut-off for win
+# Do we need to show board each round for comp guessing?
 
 # next_board = Board.new
 
